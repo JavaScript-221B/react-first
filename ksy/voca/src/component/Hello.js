af05383deb2c407109c722744eb58300e788b7db
SooYeonKim <lovesu5632@gmail.com>
2022-03-09 18:07:56 +0900

    Add: 05. CSS 작성법(module css)

--- ksy/voca/src/component/Hello.js
+++ ksy/voca/src/component/Hello.js
@@ -7,13 +7,22 @@
 
 
 import World from './World';
+import styles from "./Hello.module.css";
 
 export default function Hello() {
     return (
         <div>
-            <h2>Hello</h2>
-            <World />
-            <World />
+            <h1 style={
+                {
+                    color: '#f00',
+                    borderRight: '2px solid #000',
+                    marginBottom: '30px',
+                    opacity : 0.5
+                }
+            }>
+                Hello
+            </h1>
+            <div className={styles.box}>Hello</div>
         </div>
     );
 }
