5364e33186e1fa2b98aee31aed546285358cbfc3
SooYeonKim <lovesu5632@gmail.com>
2022-03-09 18:19:50 +0900

    Add: 06. 이벤트 처리(Handling Events)

--- ksy/voca/src/component/Hello.js
+++ ksy/voca/src/component/Hello.js
@@ -1,28 +1,32 @@
-// const Hello = () => {
-//     <p>Hello</p>;
-// };
-
-// export default Hello;
-
-
-
-import World from './World';
-import styles from "./Hello.module.css";
-
 export default function Hello() {
+    function showName() {
+        console.log("Mike");
+    }
+    function showAge(age) {
+        console.log(age);
+    }
+    function showText(txt) {
+        console.log(txt);
+    }
+
     return (
         <div>
-            <h1 style={
-                {
-                    color: '#f00',
-                    borderRight: '2px solid #000',
-                    marginBottom: '30px',
-                    opacity : 0.5
-                }
-            }>
-                Hello
-            </h1>
-            <div className={styles.box}>Hello</div>
+            <h1>Hello</h1>
+            <button onClick={showName}>Show name</button>
+            <button
+                onClick={() => {
+                    showAge(30);
+                }}
+            >
+                Show age
+            </button>
+            <input
+                type="text"
+                onChange={e => {
+                    const txt = e.target.value;
+                    showText(txt);
+                }}
+            />
         </div>
     );
 }
