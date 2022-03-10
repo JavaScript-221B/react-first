d00602f4f9d77feaddedeea4d501ceb5644adcbe
SooYeonKim <lovesu5632@gmail.com>
2022-03-10 20:30:06 +0900

    Add: 08. props

--- ksy/voca/src/component/Hello.js
+++ ksy/voca/src/component/Hello.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import UserName from './UserName';
 
-export default function Hello() {
+export default function Hello({age}) {
     /*
     function showName() {
         console.log("Mike");
@@ -38,6 +39,8 @@ export default function Hello() {
     
     //let name = "Mike"; //이 컴포넌트가 관리하고 있는 상태값이 아니라 단순 변수
     const [name, setName] = useState('Mike'); //setName 함수가 호출되서 name이 바뀌면 이 컴포넌트를 다시 렌더링해줌
+    // const [age, setAge] = useState(props.age);
+    const msg = age > 19 ? "성인 입니다." : "미성년자 입니다.";
 
     // function changeName() {
     //     // document.getElementById("name").innerText = name;
@@ -47,9 +50,11 @@ export default function Hello() {
     return (
         <div>
             <h1>state</h1>
-            <h2 id="name">{name}</h2>
+            <h2 id="name">{name}({age})</h2>
+            <UserName name={name} />
             <button onClick={() => {
-                setName(name === "Mike" ? "Jane" : "Mike")
+                setName(name === "Mike" ? "Jane" : "Mike");
+                // setAge(age + 1);
             }}>Change</button>
         </div>
     )
