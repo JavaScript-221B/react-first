044c5284273d73a4a862127b35fe1412020acc7f
SooYeonKim <lovesu5632@gmail.com>
2022-03-10 20:19:42 +0900

    Add: 07. state, useState

--- ksy/voca/src/component/Hello.js
+++ ksy/voca/src/component/Hello.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export default function Hello() {
+    /*
     function showName() {
         console.log("Mike");
     }
@@ -29,4 +32,25 @@ export default function Hello() {
             />
         </div>
     );
+    */
+    
+
+    
+    //let name = "Mike"; //이 컴포넌트가 관리하고 있는 상태값이 아니라 단순 변수
+    const [name, setName] = useState('Mike'); //setName 함수가 호출되서 name이 바뀌면 이 컴포넌트를 다시 렌더링해줌
+
+    // function changeName() {
+    //     // document.getElementById("name").innerText = name;
+    //     setName(name === "Mike" ? "Jane" : "Mike")
+    // }
+
+    return (
+        <div>
+            <h1>state</h1>
+            <h2 id="name">{name}</h2>
+            <button onClick={() => {
+                setName(name === "Mike" ? "Jane" : "Mike")
+            }}>Change</button>
+        </div>
+    )
 }
