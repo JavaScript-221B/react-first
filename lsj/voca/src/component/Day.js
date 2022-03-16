9028bc46428cc540421f79f2be561e79833cb496
이성재 <xpsxm468@naver.com>
2022-03-16 23:32:31 +0900

    Add: 10. 라우터 구현 react-router-dom

--- lsj/voca/src/component/Day.js
+++ lsj/voca/src/component/Day.js
@@ -1,20 +1,24 @@
 import dummy from '../db/data.json';
+import { useParams } from 'react-router-dom';
 
 function Day(){
-  const day = 1;
-  const wordList = dummy.words.filter(word => word.day === day);
+  const { day } = useParams();
+  const wordList = dummy.words.filter(word => word.day === Number(day));
 
   return (
-    <table>
-      <tbody>
-        {wordList.map(word => (
-          <tr key={word.id}>
-            <td>{word.eng}</td>
-            <td>{word.kor}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {wordList.map(word => (
+            <tr key={word.id}>
+              <td>{word.eng}</td>
+              <td>{word.kor}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 }
 
--- lsj/voca/src/component/DayList.js
+++ lsj/voca/src/component/DayList.js
@@ -1,4 +1,5 @@
 import dummy from '../db/data.json';
+import { Link } from 'react-router-dom';
 
 function DayList(){
   console.log(dummy);
@@ -6,7 +7,9 @@ function DayList(){
     <ul className="listDay">
       {dummy.days.map(day => (
         <li key={day.id}>
-          Day {day.day}
+          <Link to={`/day/${day.day}`}>
+            Day {day.day}
+          </Link>
         </li>
       ))}
     </ul>
