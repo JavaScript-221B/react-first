0e7553458d97c7d1858184f4052c944e18fc51b0
SooYeonKim <lovesu5632@gmail.com>
2022-03-18 23:40:57 +0900

    Add: 15. POST(생성), useHistory()

--- ksy/voca/src/App.js
+++ ksy/voca/src/App.js
@@ -1,4 +1,6 @@
 // import './App.css';
+import CreateDay from './component/CreateDay';
+import CreateWord from './component/CreateWord';
 import Day from './component/Day';
 import DayList from './component/DayList';
 import Header from './component/Header';
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
           <Route path="*" element={<EmptyPage />} />
         </Routes>
       </div>
