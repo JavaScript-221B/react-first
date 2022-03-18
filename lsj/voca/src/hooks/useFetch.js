d83731c0f3c953d4d2573e688c07c59d72a9a665
이성재 <xpsxm468@naver.com>
2022-03-18 20:46:38 +0900

    Fix:  13. Custom Hooks

--- lsj/voca/src/hooks/useFetch.js
+++ lsj/voca/src/hooks/useFetch.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 export default function useFetch(url) {
   const [data, setData] = useState([]);
 
-  const fetchData = async () => {
+  const fetchData = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
     setData(data);
