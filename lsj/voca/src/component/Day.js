85ffd6e3ea51b7f89ebd1314b68fac1b01143907
이성재 <xpsxm468@naver.com>
2022-03-18 20:39:57 +0900

    Add: 12. useEffect, fetch()로 API 호출

--- lsj/voca/src/component/Day.js
+++ lsj/voca/src/component/Day.js
@@ -1,17 +1,36 @@
-import dummy from '../db/data.json';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Word from './Word';
 
 function Day(){
   const { day } = useParams();
-  const wordList = dummy.words.filter(word => word.day === Number(day));
+  // const wordList = dummy.words.filter(word => word.day === Number(day));
+  const [words, setWords] = useState([]);
+
+  const fetchWords = async (day) => {
+    const res = await fetch(`http://localhost:3003/words?day=${day}`);
+    const data = await res.json();
+    setWords(data);
+  }
+
+  useEffect(() => {
+    fetchWords(day);
+  }, [day])
+
+  // useEffect(() => {
+  //   fetch(`http://localhost:3003/words?day=${day}`)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setWords(data);
+  //     })
+  // }, [day])
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map(word => (
+          {words.map(word => (
             <Word word={word} key={word.id}></Word>
           ))}
         </tbody>
--- lsj/voca/src/component/DayList.js
+++ lsj/voca/src/component/DayList.js
@@ -1,11 +1,34 @@
-import dummy from '../db/data.json';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function DayList(){
-  console.log(dummy);
+  const [days, setDays] = useState([]);
+
+  // async await
+  const fetchDays = async () => {
+    const res = await fetch('http://localhost:3003/days');
+    const json = await res.json();
+    setDays(json);
+  }
+
+  useEffect(() => {
+    fetchDays()
+  }, [])
+
+  // then chaining
+  // useEffect(() => {
+  //   console.log('change');
+  //   fetch('http://localhost:3003/days')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setDays(data);
+  //     })
+  //   console.log(days)
+  // }, []);
+
   return (
     <ul className="listDay">
-      {dummy.days.map(day => (
+      {days.map(day => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}>
             Day {day.day}
